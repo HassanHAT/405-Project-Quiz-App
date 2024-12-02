1c032b809e1bdfd6a3ba224fe959b91f185978cc
Hassan Turkistani <turkistani95@gmail.com>
2024-12-02 23:12:47 +0300

    Changed Files

---
+++ src/components/Home.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react"
+
+const Home = () =>{
+    const [difficulty , setDifficulty] = useState(null);
+    const [category , setCategory] = useState(null);
+    const [limit , setLimit] = useState(10);
+
+
+    return(
+        <div className="home-container">
+            
+        </div>
+    );
+
+};
---
+++ src/components/Quiz.jsx
@@ -0,0 +1,18 @@
+import { useEffect, useState } from "react"
+
+const Quiz = () =>{
+    const [questions, setQuestions] = useState([]);
+    const [answers, setAnswers] = useState({});
+
+    useEffect(() => {
+        const fetchQuestions = async () => {
+            const response = await fetch(
+
+            );
+            const data = await response.json();
+            setQuestions(data);
+        }
+    }
+
+    )
+};
