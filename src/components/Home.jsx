e9707142ed7bbafbbd04f62a706d6cefbb717de4
Hassan Turkistani <turkistani95@gmail.com>
2024-12-03 02:01:13 +0300

    second changed commit

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -8,7 +8,24 @@ const Home = () =>{
 
     return(
         <div className="home-container">
-            
+            <h1>Create Quiz</h1>
+            <h2>Select your quiz options</h2>
+            <label>
+                Difficulty:
+                <select
+                value = {difficulty}
+                onChange={(e) => setDifficulty(e.target.value)}
+                >
+                    <option value="">Select</option>
+
+                    <option value="easy">Easy</option>
+                    
+                    <option value="medium">Medium</option>
+                    
+                    <option value="hard">Hard</option>
+                </select>
+                <br />
+            </label>
         </div>
     );
 
